0cc9b9bedb1ec230d25ecaed05b209b0632b8894
Jan Zboril <jan.zboril.18@gmail.com>
2023-02-16 21:55:26 +0100

    first web idea

---
+++ proj2/app.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// vars
+const title = {
+  title: 'Vulnerabilities Educational Website',
+  subtitle: {
+    index: 'Home',
+    about: 'About',
+    xss: 'XSS'
+  }
+};
+const author = 'Jan Zbořil';
+const school = 'Brno University of Technology, <br> Faculty of Information Technology';
+const year = '2023';
+const fitlogo = 'images/VUT-FIT-logo.png';
+
+
+// static files
+app.use(express.static('public'));
+app.use('/styles', express.static(__dirname + 'public/styles'));
+app.use('/scripts', express.static(__dirname + 'public/scripts'));
+app.use('/images', express.static(__dirname + 'public/images'));
+
+
+// set views
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('', (req, res) => { 
+  res.render('index', { title,author,school,year,fitlogo } );
+});
+
+app.get('/about', (req, res) => { 
+  res.render('about', { title,author,school,year });
+});
+
+app.get('/xss', (req, res) => { 
+  res.render('xss', { title,author,school,year });
+});
+
+
+app.listen(port, () => {
+  console.info(`Example app listening on port ${port}!`);
+});
